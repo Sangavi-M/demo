a47d8eb05bdd4679bac37b59dc2bea7d245693cf
Sangavi-M <168508923+Sangavi-M@users.noreply.github.com>
2024-11-22 11:24:56 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,11 @@
-class demo
-{
-    public static void main(String []args)
-    {
-        System.out.println("My First  Program.");
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class demop {
+
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));  // Test that 2 + 3 equals 5
     }
-};
+}
